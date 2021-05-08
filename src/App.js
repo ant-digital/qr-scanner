a989a35fad8d3e826406848b7574ff26e3ef29c7
christianto <christianto@ematicsolutions.com>
2021-05-08 22:37:26 +0700

    add storage

--- src/App.js
+++ src/App.js
@@ -5,29 +5,74 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [data, setData] = useState('str')
-    function handleScan(data) {
-      console.log('DT',data)
-        setData(data)
+  // const [data, setData] = useState('str')
+  const [input, setInput] = useState('')
+  const [guestList, setGuestList] = useState(['none'])
+
+
+  function handleScan(data) {
+    console.log('DT', data)
+    setInput(data)
+    if (data) {
+      if (guestList.find(el => el === data) < 0) {
+        alert('Checked In')
+        save()
+      } else {
+        alert('Already in guest list')
+      }
     }
-    function handleError(err) {
-        console.error(err)
+  }
+  function handleError(err) {
+    console.error(err)
+  }
+
+  function save() {
+    let sessionList = sessionStorage.getItem('guestList')
+    if (sessionList) {
+      sessionList = JSON.parse(sessionList)
+    } else {
+      sessionList = []
     }
+    sessionList.push(input)
+    sessionStorage.setItem('guestList', JSON.stringify(sessionList))
+
+    const newList = guestList.concat([input])
+    setGuestList(newList)
+  }
+
+  // function handleInput(e) {
+  //   setInput(e.target.value)
+  // }
   return (
-    <div className="App">
-      
-            <h1>Scanner</h1>
-            <QrReader
-                delay={200}
-                style={{
-                    height: 240,
-                    width: 320,
-                }}
-                onError={handleError}
-                onScan={handleScan}
-            />
-            <p>QR result : {data}</p>
-        
+    <div>
+      <div className="App">
+
+        <h1>Scanner</h1>
+        <QrReader
+          delay={200}
+          style={{
+            height: 240,
+            width: 320,
+          }}
+          onError={handleError}
+          onScan={handleScan}
+        />
+        {/* <input onChange={handleInput}></input>
+        <button onClick={save}>save</button> */}
+        <p>QR result : {input}</p>
+
+
+      </div>
+      <div>
+        <h1>Guest List</h1>
+        <ul style={{ textAlign: 'left' }}>
+          {guestList.map(el => {
+            return (<li key={el}>
+              {el}
+            </li>)
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
